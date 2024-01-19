ef97dd4b593559f2c7af10d362e0ade729ad433c
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2024-01-19 13:26:05 +0900

    뷰 사이클 테스트 코드 구현

---
+++ 학습활동 코드/viewCycle/viewCycle/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  viewCycle
+//
+//  Created by Roh on 1/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+}
+
