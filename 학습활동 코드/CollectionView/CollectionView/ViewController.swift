7d28c09dd37efadcc25b900d3a6ab315dda791d1
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2024-02-15 14:35:19 +0900

    학습활동 CoreLocation 사용법

--- 학습활동 코드/CollectionView/CollectionView/ViewController.swift
+++ 학습활동 코드/CollectionView/CollectionView/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    let testwefwef = "string"
     lazy var collectionView: UICollectionView = {
 //        let layout = UICollectionViewLayout()   // 추상클래스
+        
+        // MARK: flow layout 방식
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.scrollDirection = .horizontal    // 가로로 흐를수 있음
+        
+        // MARK: configure layout 방식
+//        let configure = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+//        let layout = UICollectionViewCompositionalLayout.list(using: configure)
+        
         let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
         return collectionView
     }()
@@ -22,7 +29,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(collectionView)
-        collectionView.backgroundColor = .black
+        collectionView.backgroundColor = .cyan
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "myCell")
@@ -37,20 +44,24 @@ extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
         
+        for view in cell.contentView.subviews {
+            view.removeFromSuperview()
+        }
+        
+        cell.backgroundColor = .black
         let numberLabel = UILabel()
         numberLabel.text = indexPath.row.description
         numberLabel.textColor = .white
         numberLabel.textAlignment = .center
+        numberLabel.frame = cell.bounds
         
         cell.contentView.addSubview(numberLabel)
-        numberLabel.frame = cell.bounds
         return cell
     }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-     
         let randomWidth = Int.random(in: 1...100)
         let randomHeight = Int.random(in: 1...100)
         return CGSize(width: randomWidth, height: randomHeight)
---
+++ 학습활동 코드/CoreLocation_0215/CoreLocation_0215/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  CoreLocation_0215
+//
+//  Created by Roh on 2/15/24.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    // 위치 데이터를 코드
+    let locationManager = CLLocationManager()
+    
+//    static let shared = LocationService()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        locationManager.delegate = self
+        
+        // 권한 요청
+        locationManager.requestAlwaysAuthorization()
+
+        // 위치 업데이트 시작
+        locationManager.startUpdatingLocation()
+        view.backgroundColor = .systemPink
+        
+        let cityName = "애플 본사"
+        let geocoder = CLGeocoder()         // 주소를 변환해줌
+        geocoder.geocodeAddressString(cityName) { (placemark, error) in
+          guard error == nil else {return print(error!.localizedDescription)}//에러가 없으면 아래코드 실행
+          guard let location = placemark?.first?.location else { return print("데이터가 없습니다.")}
+          print(location.coordinate.latitude)
+          print(location.coordinate.longitude)
+        }
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 위치를 받아서 필요한 코드를 작성
+        let newLocation = locations.last
+        print(newLocation)
+        
+    }
+    
+    
+}
